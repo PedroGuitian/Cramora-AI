fb6415f2486580af593fe4b981b9ab2d81f39fdc
PedroGuitian <pedroguitiperez@gmail.com>
2025-04-09 12:39:03 -0400

    added Test Question model

--- cram_app/models.py
+++ cram_app/models.py
@@ -10,3 +10,11 @@ class CramSheet(models.Model):
 
     def __str__(self):
         return f"{self.title} - {self.user.username}"
+
+class TestQuestion(models.Model):
+    cram_sheet = models.ForeignKey('CramSheet', on_delete=models.CASCADE, related_name='questions')
+    question_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Q for: {self.cram_sheet.title}"
