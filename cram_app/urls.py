6544451fb2a6b4010cb47d5ed6cf3661ca4d2d33
PedroGuitian <pedroguitiperez@gmail.com>
2025-04-22 21:29:08 -0400

    Added Question Generator

--- cram_app/urls.py
+++ cram_app/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path("cram-hub/<int:hub_id>/", views.cram_hub_dashboard, name="cram_hub_dashboard"),
     path("cram-hub/<int:hub_id>/generate-cram-sheet/", views.generate_cram_sheet, name="generate_cram_sheet"),
     path("cram-hub/<int:hub_id>/generate-test-questions/", views.generate_test_questions, name="generate_test_questions"),
-    path('cram-hub/<int:hub_id>/add-files/', views.add_files_to_hub, name='add_files_to_hub')
+    path('cram-hub/<int:hub_id>/add-files/', views.add_files_to_hub, name='add_files_to_hub'),
+    path("question/<int:question_id>/edit/", views.edit_question, name="edit_question")
 ]
